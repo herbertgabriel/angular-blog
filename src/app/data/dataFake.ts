d8af53a62af67cd0b3e1913b6d7e270fd9299273
Herbert Gabriel <herbertgacruz@gmail.com>
2025-04-03 10:39:40 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Ainda Estou Aqui",
+    "description": "Ainda Estou Aqui é uma adaptação cinematográfica do livro autobiográfico de Marcelo Rubens Paiva, que narra a emocionante trajetória de sua mãe, Eunice Paiva, durante a ditadura militar no Brasil. Ambientada em 1970, a história retrata como a vida de uma mulher casada com um importante político muda drasticamente após o desaparecimento dele, capturado pelo regime militar. Forçada a abandonar a rotina de dona de casa, Eunice (Fernanda Torres/Fernanda Montenegro) se transforma em uma ativista dos direitos humanos, lutando pela verdade sobre o paradeiro do marido e enfrentando as consequências brutais da repressão. O filme explora não apenas o drama pessoal de Eunice, mas também o impacto do regime militar na vida de milhares de famílias brasileiras, destacando o papel das mulheres na resistência. Com uma narrativa profunda e sensível, Ainda Estou Aqui traz à tona questões de perda, coragem e resiliência, enquanto revisita um dos períodos mais sombrios da história do Brasil. A obra é um tributo à força de Eunice Paiva, que, contra todas as adversidades, se torna uma figura central na luta pelos direitos humanos no país.",
+    "photoCover":"https://imgs.search.brave.com/4bMjtajzo9EFHC0XiNWq6k6SNySPqQV0ohBrKpCdvEE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bm9zLnB0L2NpbmVt/YXMvbW92aWVzL2Zp/bGVzLzEwODB4NTc2/L2JhMDQ1YjI1LTJm/NGMtNGRiOC1hZmE5/LWE5NzI1NjNlODQy/NF9Ub3AxMC5qcGc"
+  },
+  {
+    "id":"2",
+    "title": "Vitória",
+    "description": "É um filme de drama brasileiro de 2023, dirigido por André Novais Oliveira. A história gira em torno de Vitória, uma mulher que vive em um mundo onde a vida é marcada por desafios e superações. O filme explora temas como a luta pela sobrevivência, a busca por identidade e a força das relações humanas. Com uma narrativa sensível e poética, Vitória nos convida a refletir sobre as complexidades da vida e a importância de encontrar beleza mesmo nas situações mais difíceis.",
+    "photoCover":"https://imgs.search.brave.com/vI_s_l-V_Ld8rGnXr3GPBjucKfciutNB7CyoguU7fRk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9qcGlt/Zy5jb20uYnIvdXBs/b2Fkcy8yMDI1LzAx/L2Zlcm5hbmRhLW1v/bnRlbmVncm8tNzUw/eDQzMC5wbmc"
+  }
+  ,
+  {
+    "id":"3",
+    "title": "Arcane",
+    "description": "Arcane é uma série de animação baseada no universo do jogo League of Legends, que explora a origem de personagens icônicos e os conflitos entre as cidades de Piltover e Zaun. A trama segue a história de duas irmãs, Vi e Jinx, enquanto elas navegam por um mundo repleto de intrigas políticas, desigualdade social e magia. Com uma animação deslumbrante e uma narrativa envolvente, Arcane mergulha os espectadores em um universo rico e complexo, onde as escolhas pessoais têm consequências profundas.",
+    "photoCover":"https://imgs.search.brave.com/DLPD4pCbza26Ueee0d2bLEHFvJo_S53lk3FTtFVqhOE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zMi10/ZWNodHVkby5nbGJp/bWcuY29tL3hVZE12/T2dBR3FBQUYwaFVF/SFF1U2dLWE5Vdz0v/MHgwOjEzNzB4Nzcx/Lzk4NHgwL3NtYXJ0/L2ZpbHRlcnM6c3Ry/aXBfaWNjKCkvaS5z/My5nbGJpbWcuY29t/L3YxL0FVVEhfMDhm/YmY0OGJjMDUyNDg3/Nzk0M2ZlODZlNDMw/ODdlN2EvaW50ZXJu/YWxfcGhvdG9zL2Jz/LzIwMjEvYi9LL0Zq/QlF6NlJsMklRbDBW/N2V2WUt3L2FyY2Fu/ZS1maW5hbC1wb3N0/ZXItMTZ4OS1uby10/ZXh0LW5vLWJvcmRl/ci5qcGc"
+  }
+  ,
+  {
+    "id":"4",
+    "title": "CastleVania Nocturne",
+    "description": "CastleVania Nocturne é uma série de animação que continua a história do universo de Castlevania, explorando novos personagens e desafios em um mundo repleto de criaturas sobrenaturais. A trama se passa em um cenário gótico e sombrio, onde caçadores de vampiros lutam contra forças malignas para proteger a humanidade. Com uma animação estilizada e uma narrativa envolvente, CastleVania Nocturne mergulha os espectadores em um universo rico em mitologia e ação.",
+    "photoCover": "https://imgs.search.brave.com/E3lO89furNNjYMnyoiH3nFDZxFKo2uVXviUxOUL0yRY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMtcHJkLmlnbmlt/Z3MuY29tLzIwMjUv/MDEvMTUvY24tczIt/YmxvZ3JvbGwtMTcz/Njk4MjExMDM2MS5q/cGc_d2lkdGg9Mzg0/MA"
+  }
+]
